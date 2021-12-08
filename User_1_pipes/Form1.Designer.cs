22fa0e26e71f50a848536952ad4a79e066319657
Hryhorenko Bohdan <91735886+BohdanGL@users.noreply.github.com>
2021-12-08 15:22:49 +0200

    Add files via upload

---
+++ User_1_pipes/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace LabOS_11_User_1_sockets
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Send = new System.Windows.Forms.Button();
+            this.textBox_Message = new System.Windows.Forms.TextBox();
+            this.textBox_Chat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(127, 323);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(94, 29);
+            this.button_Send.TabIndex = 5;
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // textBox_Message
+            // 
+            this.textBox_Message.Location = new System.Drawing.Point(49, 276);
+            this.textBox_Message.Name = "textBox_Message";
+            this.textBox_Message.Size = new System.Drawing.Size(253, 27);
+            this.textBox_Message.TabIndex = 4;
+            // 
+            // textBox_Chat
+            // 
+            this.textBox_Chat.Location = new System.Drawing.Point(49, 24);
+            this.textBox_Chat.Multiline = true;
+            this.textBox_Chat.Name = "textBox_Chat";
+            this.textBox_Chat.ReadOnly = true;
+            this.textBox_Chat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Chat.Size = new System.Drawing.Size(253, 223);
+            this.textBox_Chat.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 377);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.textBox_Message);
+            this.Controls.Add(this.textBox_Chat);
+            this.Name = "Form1";
+            this.Text = "User_1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.TextBox textBox_Message;
+        private System.Windows.Forms.TextBox textBox_Chat;
+    }
+}
+
---
+++ User_1_pipes/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabOS_11_User_1_sockets
+{
+    public partial class Form1 : Form
+    {
+        private NamedPipeServerStream pipeServer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            pipeServer =
+                new NamedPipeServerStream("Pipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+
+            // Wait for a client to connect
+            pipeServer.WaitForConnection();
+
+            InitTimer();
+
+        }
+
+        private Timer timer;
+        public void InitTimer()
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 100; // in miliseconds
+            timer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetMessage();
+        }
+
+        public void GetMessage()
+        {
+            if (new FileInfo(@"C:\Users\User\source\repos\LabOS_11_User_1_sockets\LabOS_11_User_1_sockets\pipes_2.txt").Length == 0)
+            {
+                return;
+            }
+
+            StreamReader sr = new StreamReader(pipeServer);
+
+            // Display the read text to the console
+            string temp;
+
+            if((temp = sr?.ReadLine()) != null)
+            {
+                textBox_Chat.Text += "User_2: " + temp;
+
+                File.WriteAllText(@"C:\Users\User\source\repos\LabOS_11_User_1_sockets\LabOS_11_User_1_sockets\pipes_2.txt", string.Empty);
+
+                textBox_Chat.Text += System.Environment.NewLine;
+
+                sr.Close();
+
+                pipeServer =
+                new NamedPipeServerStream("Pipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+                pipeServer.WaitForConnection();
+            }
+        }
+
+        private void button_Send_Click(object sender, EventArgs e)
+        {
+            StreamWriter streamWriter =
+               new StreamWriter(@"C:\Users\User\source\repos\LabOS_11_User_1_sockets\LabOS_11_User_1_sockets\pipes.txt");
+
+            try
+            {
+                // Read user input and send that to the client process.
+
+                using (StreamWriter sw = new StreamWriter(pipeServer))
+                {
+                    sw.AutoFlush = true;
+                    streamWriter.WriteLine("Data pushed!");
+                    streamWriter.Close();
+                    textBox_Chat.Text += "User_1: " + textBox_Message.Text;
+                    textBox_Chat.Text += System.Environment.NewLine;
+                    sw.WriteLine(textBox_Message.Text);
+                }
+            }
+            // Catch the IOException that is raised if the pipe is broken
+            // or disconnected.
+            catch (IOException ex)
+            {
+                Console.WriteLine("ERROR: {0}", ex.Message);
+            }
+            pipeServer =
+                new NamedPipeServerStream("Pipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+            pipeServer.WaitForConnection();
+        }
+    }
+}
