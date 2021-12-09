630176155ea304aa95c801913152b28c5ad0a827
VitaliiHeorhiievskyi <91679829+VitaliiiHeorhiievskyi@users.noreply.github.com>
2021-12-09 22:48:44 +0200

    Add DataBase(Pipes)

--- User_1_pipes/Form1.cs
+++ User_1_pipes/Form1.cs
@@ -95,8 +95,7 @@ namespace LabOS_11_User_1_sockets
             {
                 Console.WriteLine("ERROR: {0}", ex.Message);
             }
-            pipeServer =
-                new NamedPipeServerStream("Pipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
+            pipeServer = new NamedPipeServerStream("Pipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte);
             pipeServer.WaitForConnection();
         }
     }
